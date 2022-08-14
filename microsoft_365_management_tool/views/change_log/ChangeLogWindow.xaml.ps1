ccc427faae7d73c07d727be12ce561acc1957497
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2022-08-14 03:24:10 +0100

    frontend: Add change log window.

---
+++ microsoft_365_management_tool/views/change_log/ChangeLogWindow.xaml.ps1
@@ -0,0 +1,16 @@
+﻿[System.Reflection.Assembly]::LoadWithPartialName("PresentationFramework") | Out-Null
+
+Function Import-Xaml {
+	[xml]$Xaml = Get-Content -Path $PSScriptRoot\ChangeLogWindow.xaml
+	$Manager = New-Object System.Xml.XmlNamespaceManager -ArgumentList $Xaml.NameTable
+	$Manager.AddNamespace("x", "http://schemas.microsoft.com/winfx/2006/xaml");
+	$XamlReader = New-Object System.Xml.XmlNodeReader $Xaml
+	[Windows.Markup.XamlReader]::Load($XamlReader)
+}
+
+$ChangeLogWindow = Import-Xaml
+
+$ChangeLogWindowObjects = [PSCustomObject]@{
+	ChangeLogTextBox = $ChangeLogWindow.FindName('ChangeLogTextBox')
+	CloseButton = $ChangeLogWindow.FindName('CloseButton')
+}
