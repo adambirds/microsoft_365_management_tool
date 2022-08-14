96dac25282627298dbe8c39881c1d558ee5b40b0
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2022-08-14 03:24:40 +0100

    backend: Add code for opening and closing change log window.

--- microsoft_365_management_tool/globals/GlobalFunctions.ps1
+++ microsoft_365_management_tool/globals/GlobalFunctions.ps1
@@ -1,5 +1,14 @@
 . $PSScriptRoot\..\loaders\WindowLoaders.ps1
 
 $MainMenuObjects.MenuBarExitButton.Add_Click( {
+	$ChangeLogWindow.Close()
 	$MainMenuWindow.Close()
+} )
+
+$MainMenuObjects.MenuBarChangeLogButton.Add_Click( {
+	$ChangeLogWindow.ShowDialog()
+} )
+
+$ChangeLogWindowObjects.CloseButton.Add_Click( {
+	$ChangeLogWindow.Close()
 } )
--- microsoft_365_management_tool/loaders/WindowLoaders.ps1
+++ microsoft_365_management_tool/loaders/WindowLoaders.ps1
@@ -1,1 +1,2 @@
 . $PSScriptRoot/../views/main_menu/MainMenuWindow.xaml.ps1
+. $PSScriptRoot/../views/change_log/ChangeLogWindow.xaml.ps1
