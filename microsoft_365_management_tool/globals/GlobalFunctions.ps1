dfe040b850e704111eb161ca94ca7c83abf8c171
Adam Birds <adam.birds@adbwebdesigns.co.uk>
2022-08-14 13:25:48 +0100

    backend: Add change log functions and version message box.

--- microsoft_365_management_tool/globals/GlobalFunctions.ps1
+++ microsoft_365_management_tool/globals/GlobalFunctions.ps1
@@ -1,12 +1,27 @@
 . $PSScriptRoot\..\loaders\WindowLoaders.ps1
+. $PSScriptRoot\GlobalVariables.ps1
+. $PSScriptRoot\..\utils\New-MessageBox.ps1
 
 $MainMenuWindowObjects.MenuBarExitButton.Add_Click( {
 	$ChangeLogWindow.Close()
 	$MainMenuWindow.Close()
 } )
 
+$MainMenuWindowObjects.ExitButton.Add_Click( {
+	$ChangeLogWindow.Close()
+	$MainMenuWindow.Close()
+} )
+
 $MainMenuWindowObjects.MenuBarChangeLogButton.Add_Click( {
 	$ChangeLogWindow.ShowDialog()
+} )
+
+$MainMenuWindowObjects.MenuBarVersionButton.Add_Click( {
+	New-MessageBox -Message ("Version: {0}" -f $VersionNo) -Title "Microsoft 365 Management Tool | Version Details" -Buttons OK -ShowOnTop -Icon Information
+} )
+
+$ChangeLogWindow.Add_Loaded( {
+	$ChangeLogWindowObjects.ChangeLogTextBox.Text = $ChangeLog
 } )
 
 $ChangeLogWindowObjects.CloseButton.Add_Click( {
---
+++ microsoft_365_management_tool/globals/GlobalVariables.ps1
@@ -0,0 +1,8 @@
+$VersionNo = "v1.0.0"
+$ChangeLog = @"
+Change Log:
+
+Version 1.0.0:
+
+* Initial Release
+"@
